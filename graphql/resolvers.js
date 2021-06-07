5996e359a18d5f0ed8f5f8cebc5fff237b858437
Zatt Wine <tomclick.org@gmail.com>
2021-06-07 11:31:17 +0630

    add a mutation resolver for creating user

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,1 +1,37 @@
-module.exports = {};
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+module.exports = {
+  createUser: async function ({ userInput }, req) {
+    // const email = args.userInput.email;
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("User exists already.");
+      throw error;
+    }
+
+    // rounds=8 : ~40 hashes/sec
+    // rounds=9 : ~20 hashes/sec
+    // rounds=10: ~10 hashes/sec
+    // rounds=11: ~5  hashes/sec
+    // rounds=12: 2-3 hashes/sec
+    // rounds=13: ~1 sec/hash
+    // rounds=14: ~1.5 sec/hash
+    // rounds=15: ~3 sec/hash
+    // rounds=25: ~1 hour/hash
+    // rounds=31: 2-3 days/hash
+    const saltRound = 12; // recommand
+    const hashedPass = await bcrypt.hash(userInput.password, saltRound);
+    const user = new User({
+      email: userInput.email,
+      password: hashedPass,
+      name: userInput.name,
+    });
+    const createdUser = await user.save();
+
+    return {
+      ...createdUser._doc,
+      _id: createdUser._id.toString(),
+    };
+  },
+};
